af246a78585ab50ab92955e7ff67c5cb63845c50
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-11-06 02:43:50 +0300

    Update views.py
    created index, profile, register

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http  import HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate, logout
+from .forms import SignupForm,UpdateUserForm,UpdateProfileForm,NeighbourHoodForm,BusinessForm, PostForm
+from .models import Profile, User, Neighbourhood, Business, Post
+
 
 # Create your views here.
+
+def index(request):
+     return render(request, 'index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form}) 
+
+
+
+def profile(request, username):
+    return render(request, 'profile.html')
+
+
