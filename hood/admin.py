d54abbf84157446ce333f0345a025f6377cdca7b
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-11-06 02:28:25 +0300

    Update admin.py
    Registered all the models

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Neighborhood, Post,Profile,Business
 
 # Register your models here.
+admin.site.register(Neighborhood)
+admin.site.register(Post)
+admin.site.register(Profile)
+admin.site.register(Business)
