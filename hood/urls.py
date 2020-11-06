e3ca87139e0af2baa74412440944eb4933207641
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-11-06 03:52:39 +0300

    Update urls.py
    Imported all view functions

--- hood/urls.py
+++ hood/urls.py
@@ -1,11 +1,34 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-from . import views
+from .views import (
+    index,
+    register,
+    profile,
+    edit_profile,
+    create_hood,
+    allhoods,
+    join_a_hood,
+    leave_hood,
+    hood,
+    hoodpopulation,
+    business,
+    post,
+    search,
+)
 
 urlpatterns=[
-    path('',views.index,name = 'index'),
-    path('register/', views.register, name='register'),
+    path('',index,name = 'index'),
+    path('register/', register, name='register'),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path('profile/<username>/', views.profile, name='profile'),
-
+    path('profile/<username>/', profile, name='profile'),
+    path('profile/<username>/edit', edit_profile, name='edit'),
+    path('create-a-hood/', create_hood, name='create_a_hood'),
+    path('allhoods/', allhoods, name='allhoods'),
+    path('hood/<hood_id>', hood, name='hood'),
+    path('join-hood/<id>', join_a_hood, name='join'),
+    path('leave-hood/<id>', leave_hood, name='leave'),
+    path('population/<hood_id>', hoodpopulation, name='population'),
+    path('business/<hood_id>', business, name='business'),
+    path('post/<hood_id>', post, name='post'),
+    path('search/', search, name='search_results'),
 ]
