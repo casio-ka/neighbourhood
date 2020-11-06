1226f31dee4072144867f464cde194606e961335
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-11-06 18:31:26 +0300

    regisered default_app_config

--- hood/apps.py
+++ hood/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
-
+from django.utils.translation import ugettext_lazy as _
 
 class HoodConfig(AppConfig):
     name = 'hood'
+    verbose_name = ('hood')
+
+    def ready(self):
+        import hood.signals
